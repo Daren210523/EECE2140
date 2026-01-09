b92d35234cf1e214d66a8affbdd14a2f407b8e4d
Daren <xu.dareEnortheastern.edu>
2026-01-09 14:17:16 -0500

    Initial commit

--- Hello1.cpp
+++ Hello1.cpp
@@ -3,7 +3,6 @@
 int main() {
     std::cout << "Hello from EECE 2140 on WSL2!\n";
     std::cout << "This is from EECE 2140 on WSL2!\n";
-        std::cout << "This is from EECE 2140 on WSL2!\n";
 
 
 
