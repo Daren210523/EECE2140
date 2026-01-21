557446054f0fd142e89fd7502adc18bbba163e6d
Daren210523 <xu.dare@northeastern.edu>
2026-01-21 14:01:31 -0500

    Calculator

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char op;
+    double num1, num2;
 
+    //Ask for the action
+    cout << "Enter operator (+, -, *, /):";
+    cin >> op;
+
+    //Ask for two numbers
+    cout << "Enter two numbers:";
+    cin >> num1 >> num2;
+    
+    //Perform the calculation
+    switch(op) {
+        case '+':
+        cout << num1 << " + " << num2 << " = " << num1 + num2;
+        break;
+
+        case '-':
+        cout << num1 << " - " << num2 << " = " << num1 - num2;
+        break;
+
+        case '*':
+        cout << num1 << " * " << num2 << " = " << num1 * num2;
+        break;
+
+        case '/':
+        if (num2 != 0.0)
+        cout << num1 << " / " << num2 << " = " << num1 / num2;
+        else
+        cout << "Error: Division by zero is not allowed.";
+        break;
+
+    }
+}
